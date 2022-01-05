d343925b627016d54d5553e34202a91b3ed79e0a
Fahmi Alfareza <fahmialfareza@icloud.com>
2022-01-05 17:06:51 +0700

    Test Done

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -89,12 +89,10 @@ describe('Lottery Contract', () => {
     });
 
     const initialBalance = await web3.eth.getBalance(accounts[0]);
-
     await lottery.methods.pickWinner().send({ from: accounts[0] });
-
     const finalBalance = await web3.eth.getBalance(accounts[0]);
-
     const different = finalBalance - initialBalance;
+
     assert(different > web3.utils.toWei('1.8', 'ether'));
   });
 });
