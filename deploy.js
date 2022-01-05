07b2dbb9c65c198d5726e4be820ed0965dcdd5da
Fahmi Alfareza <fahmialfareza@icloud.com>
2022-01-05 17:06:00 +0700

    Test Done

---
+++ deploy.js
@@ -0,0 +1,22 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'finish initial buddy liberty blind opera popular jaguar switch ramp victory dentist',
+  'https://rinkeby.infura.io/v3/8b9216a5d2434898bfc43998c1e718bb'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+};
+deploy();
